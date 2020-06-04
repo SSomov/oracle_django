a43b9c36e66fa557afcba3a2f60f0d9c9a665700
SSomov <goofma@gmail.com>
2020-06-04 21:37:45 +0000

    Add console oracle update, add parsing upload csv, add search data

--- ordjango/main/views.py
+++ ordjango/main/views.py
@@ -3,6 +3,9 @@ from django.core.files.storage import FileSystemStorage
 from django.conf import settings
 from django.shortcuts import render
 from django.http import HttpResponse
+import json
+import csv
+from .models import TestDjango
 
 # Create your views here.
 
@@ -21,10 +24,27 @@ def search(request):
 
 def ajax_search(request):
     search = request.GET["search"]
-    return HttpResponse(search)
+    obj = TestDjango.objects.filter(id=search).first()
+    mess = "DATA_LOAD "+obj.data_load+"<br>"+"SENDER "+obj.sender+"<br>"+"RECEIVER "+obj.receiver
+    return HttpResponse(mess)
 
 
 def ajax_upload(request):
-    #this_file = request.POST["firstName"]
-    print(request.POST.firstName)
-    return HttpResponse('ok')
+    # req = json.loads(request.body.decode('utf-8'))
+    # print(req, type(req))
+    x = handle_uploaded_file(request.FILES['file'])
+    return HttpResponse(x)
+
+
+def handle_uploaded_file(f):
+    with open('media/temp.csv', 'wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
+    with open('media/temp.csv', "r", encoding='utf-8') as csvbase:
+        spamreader = csv.reader(csvbase, delimiter=';')
+        for lines in spamreader:
+            TestDjango.objects.filter(msg_id=lines[0]).update(
+                update_status=lines[1],
+                update_user=lines[2]
+            )
+        return "ok"
--- task/update_inb_oracle.py
+++ task/update_inb_oracle.py
@@ -12,13 +12,12 @@ dsn_tns = cx_Oracle.makedsn(ip, port, service_name=SID)
 connection = cx_Oracle.connect(usr, pwd, dsn_tns, encoding="UTF-8")
 cursor = connection.cursor()
 print("oracle database " + connection.version)
-with open('insert.csv', "r", encoding='utf-8') as csvbase:
+with open('fileupdate.csv', "r", encoding='utf-8') as csvbase:
     print(csvbase)
     spamreader = csv.reader(csvbase, delimiter=';')
     for lines in spamreader:
         print(lines)
-        cursor.execute("INSERT INTO TEST_DJANGO (ADAPTER,DATA_LOAD,SENDER,RECEIVER,INTERFACE_NAME,TYPE_ERROR,MSG_ID,ERROR_TEXT) VALUES (:1, :2, :3, :4, :5, :6, :7, :8)", 
-                (lines[0], lines[1], lines[2], lines[3], lines[4], lines[5], lines[6], lines[7]))
+        cursor.execute("UPDATE TEST_DJANGO SET UPDATE_STATUS = :1, UPDATE_USER = :2 WHERE MSG_ID = :3", (lines[1], lines[2], lines[0]))
 #        print(cursor.execute("SELECT * FROM ALL_TABLES"))
         connection.commit()
 cursor.close()
